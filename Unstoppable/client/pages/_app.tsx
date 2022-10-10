3496903070c09251a9e77aa865c6dfd655fe7005
berkayermis <mberkayermis@gmail.com>
2022-10-10 18:25:45 +0300

    Update _app

--- Unstoppable/client/pages/_app.tsx
+++ Unstoppable/client/pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from "../components/web3/providers";
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/Header/index'
+import Footer from '../components/Footer/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

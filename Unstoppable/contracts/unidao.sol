d4407291f3d3be23b7c06729f90e445411e7bc5e
berkayermis <mberkayermis@gmail.com>
2022-10-10 06:23:23 +0300

    Add UniDAO Smart Contract

---
+++ Unstoppable/contracts/unidao.sol
@@ -0,0 +1,105 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Unidao is Ownable {
+
+    event ApplicationSent(address indexed admin, string universityName, uint256 universityId);
+    event pollCreated (address indexed admin, string universityName, uint256 pollId);
+
+    struct Poll {
+        string universityName;
+        address admin;
+        uint256 pollId;
+        string pollDescription;
+        uint256 pollStartTime;
+        uint256 pollEndTime;
+        uint256 pollYesVotes;
+        uint256 pollNoVotes;
+        bool pollActive;
+    }
+
+    struct university {
+        address admin;
+        string universityName;
+        string universityDescription;
+        uint256 universityId;
+        string country;
+        string communityName;
+        string moderatorDomain;
+        uint totalNoOfCommunity;
+    }
+
+    mapping (uint => university) public universities;
+    mapping (uint => Poll) public polls;
+
+    uint public universityCount = 0;
+    uint public pollCount = 0;
+
+    function applicationFunction(
+                    string memory _universityName, 
+                    string memory _universityDescription,
+                    string memory _country, 
+                    string memory _communityName, 
+                    string memory _moderatorDomain, 
+                    uint _totalNoOfCommunity,
+                    string _signature,
+                    bytes32 _hashedMessage
+                    ) public {
+        expectedAddress = verifySignature(_hashedMessage, _signature);
+        require(expectedAddress == msg.sender, "Invalid signature");
+        universities[universityCount] = application(
+            msg.sender, 
+            _universityName, 
+            _universityDescription,
+            universityCount,
+            _country, 
+            _communityName, 
+            _moderatorDomain, 
+            _totalNoOfCommunity
+            );
+        emit ApplicationSent(msg.sender, _universityName, universityCount);
+        universityCount++;
+    }
+    
+    function getUniversityCount() public view returns (uint) {
+        return universityCount;
+    }
+
+    function getAllUniversities() public view returns (university[] memory) {
+        university[] memory allUniversities = new university[](universityCount);
+        for (uint i = 0; i < universityCount; i++) {
+            allUniversities[i] = universities[i];
+        }
+        return allUniversities;
+    }
+
+    function getApplication(uint _index) public view returns (university memory) {
+        return universities[_index];
+    }
+
+    function verifySignature(bytes32 _hashedMessage, bytes memory _signature) internal pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _hashedMessage));
+        address signer = ecrecover(prefixedHashMessage, uint8(_signature[0]), uint8(_signature[1]), uint8(_signature[2]));
+        return signer;
+    }
+
+    function createPoll(uint _duration, uint _universityId) public {
+        require(universities[_universityId].admin == msg.sender, "You are not the admin of this university");
+        polls[pollCount] = Poll(
+            _universityName,
+            msg.sender,
+            pollCount,
+            _pollDescription,
+            block.timestamp,
+            block.timestamp + _duration,
+            0,
+            0,
+            true
+        );
+        emit pollCreated(msg.sender, _universityName, pollCount);
+        pollCount++;
+    } 
+}

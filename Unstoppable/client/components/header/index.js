e1a210a6033db2110dfaf5e1c0a5f1a7d15d943a
berkayermis <mberkayermis@gmail.com>
2022-10-10 03:53:06 +0300

    Add header

---
+++ Unstoppable/client/components/header/index.js
@@ -0,0 +1,77 @@
+import UAuth from '@uauth/js'
+import React, { useEffect, useState } from 'react';
+import college from '../../public/college.png';
+import Image from 'next/image';
+import styles from './styles.module.css';
+import uns from '../../public/uns.webp';
+
+const Header = () => {
+    const [auth, setAuth] = useState(null);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const uauth = new UAuth({
+            clientID: process.env.CLIENT_ID,
+            redirectUri: "http://localhost:3000",
+            scope: "openid wallet",
+        }) 
+        setAuth(uauth)
+        }, [])
+
+        const logingWithUAuth = async () => {
+        try{
+            const user = await auth.loginWithPopup()
+            setUser(user)
+            console.log(user)
+        }
+        catch(err){
+            console.log(err)
+        }
+        }
+
+        const logout = async () => {
+        try{
+            const result = await auth.logout()
+            setUser(result)
+            console.log(result)
+        }
+        catch(err){
+            console.log(err)
+        }
+        }
+
+    return (
+        <header className="text-gray-600 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Image src={college} alt="college" width={60} height={60} />
+                <span className="ml-5 text-2xl text-[#6056ad]">uniDAO</span>
+                </a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <a className={styles.navLink}>Universities</a>
+                <a className={styles.navLink}>About</a>
+                <a className={styles.navLink}>Contact</a>
+                <a className={styles.navLink}>Apply</a>
+                </nav>
+                {user ? (
+                <p>xd</p> )
+                : (
+                    <button
+                onClick={logingWithUAuth}
+                className="inline-flex items-center text-white bg-[#4d47f8] border-0 py-1 px-3 focus:outline-none hover:opacity-90 rounded text-base mt-4 md:mt-0">
+                    <span className='mt-2'>
+                    <Image src={uns} width="30" height="30" />    
+                    </span>
+                    Login With Unstoppable
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+                </button>
+                )    
+                }
+            </div>
+        </header>
+    );
+    }
+
+export default Header;
--- Unstoppable/client/pages/index.js
+++ Unstoppable/client/pages/index.js
@@ -1,51 +1,10 @@
-import UAuth from '@uauth/js'
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export default function App( ) {
-  const [auth, setAuth] = useState(null);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const uauth = new UAuth({
-      clientID: process.env.CLIENT_ID,
-      redirectUri: process.env.REDIRECT_URI,
-      scope: "openid wallet",
-    }) 
-    setAuth(uauth)
-  }, [])
-
-  const logingWithUAuth = async () => {
-    try{
-      const user = await auth.loginWithPopup()
-      setUser(user)
-      console.log(user)
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
-
-  const logout = async () => {
-    try{
-      const result = await auth.logout()
-      setUser(result)
-      console.log(result)
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
 
   return (
     <div>
-      <button className='bg-green-500 p-5' onClick={logingWithUAuth}>
-        login with popup
-      </button>
-      <br />
-      <button className='bg-red-500 p-5' onClick={logout}>
-        logout
-      </button>
-      
+      <h1>hello world</h1>      
     </div>
   );
 }
